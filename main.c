ed36f22adf258e907d3f4f89f19126b2d8d31811
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-03-22 19:38:14 +0530

    Add parseAndExecute.

--- main.c
+++ main.c
@@ -9,10 +9,38 @@ static int getInput()
     return fgets(input, sizeof(input), stdin) != NULL;
 }
 
+static int parseAndExecute()
+{
+    char *verb = strtok(input, " \n");
+    char *noun = strtok(NULL, " \n");
+
+    if (verb != NULL)
+    {
+        if (strcmp(verb, "quit") == 0)
+        {
+            return 0;
+        }
+        else if (strcmp(verb, "look") == 0)
+        {
+            printf("It's too dark to see.\n");
+        }
+        else if (strcmp(verb, "go") == 0)
+        {
+            printf("It's too dark to go anywhere.\n");
+        }
+        else
+        {
+            printf("I don't know how to '%s'.\n", verb);
+        }
+    }
+    return 1;
+}
+
 int main()
 {
     printf("Welcome to CaveMe.\n");
     printf("It's very dark in here.\n");
+    while (getInput() && parseAndExecute());
 
     printf("\n Bye. \n");
     return 0;
