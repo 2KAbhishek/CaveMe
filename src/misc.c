939cde060ebbeeb27f23735fb7452313dbb1355c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-03-23 10:22:09 +0530

    Add parseObject.

--- src/misc.c
+++ src/misc.c
@@ -2,3 +2,16 @@
 #include <string.h>
 #include "object.h"
 #include "misc.h"
+
+OBJECT *parseObject(const char *noun)
+{
+    OBJECT *obj, *found = NULL;
+    for (obj = objs; obj < endOfObjs; obj++)
+    {
+        if (noun != NULL && strcmp(noun, obj->tag) == 0)
+        {
+            found = obj;
+        }
+    }
+    return found;
+}
