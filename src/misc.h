910bfd73268fe8ba5fa61ccca834ded4e045c454
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-03-23 13:50:26 +0530

    Add Distance enum

--- src/misc.h
+++ src/misc.h
@@ -1,4 +1,19 @@
+typedef enum
+{
+    distPlayer,
+    distHeld,
+    distHeldContained,
+    distLocation,
+    distHere,
+    distHereContained,
+    distOverthere,
+    distNotHere,
+    distUnknownObject,
+    distNoObjectSpecified
+} DISTANCE;
+
 extern OBJECT *getPassageTo(OBJECT *targetLocation);
 extern OBJECT *parseObject(const char *noun);
 extern OBJECT *personHere(void);
+extern DISTANCE distanceTo(OBJECT *obj);
 extern int listObjsAtLoc(OBJECT *location);
