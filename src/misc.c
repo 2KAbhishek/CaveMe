5c00e1bd4691920d2a6ffc6fa22015e96915b0a4
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-03-23 12:39:59 +0530

    Add getPassageTo

--- src/misc.c
+++ src/misc.c
@@ -3,6 +3,20 @@
 #include "object.h"
 #include "misc.h"
 
+OBJECT *getPassageTo(OBJECT *targetLocation)
+{
+    OBJECT *obj;
+    for (obj = objs; obj < endOfObjs; obj++)
+    {
+        if (obj->location == player->location &&
+            obj->destination == targetLocation)
+        {
+            return obj;
+        }
+    }
+    return NULL;
+}
+
 OBJECT *parseObject(const char *noun)
 {
     OBJECT *obj, *found = NULL;
