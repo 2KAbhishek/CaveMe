e44a16224f7955212645239af6717d443aa1436a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-03-23 12:32:04 +0530

    Add passage objects.

--- src/object.c
+++ src/object.c
@@ -2,10 +2,12 @@
 #include "object.h"
 
 OBJECT objs[] = {
-    {"a little cave", "cave", NULL},
-    {"an open field", "field", NULL},
-    {"a silver coin", "silver", field},
-    {"a gold coin", "gold", cave},
-    {"a burly guard", "guard", field},
-    {"yourself", "yourself", cave}
+    {"a little cave", "cave", NULL, NULL},
+    {"an open field", "field", NULL, NULL},
+    {"a silver coin", "silver", field, NULL},
+    {"a gold coin", "gold", cave, NULL},
+    {"a burly guard", "guard", field, NULL},
+    {"yourself", "yourself", cave, NULL},
+    {"a cave entrance", "entrance", field, cave},
+    {"a way out", "out", cave, field}
 };
