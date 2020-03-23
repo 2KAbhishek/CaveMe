3a62c49ea8b12b67ffa643c0dbef68b9f33ca411
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-03-23 10:14:26 +0530

    Add object.c

---
+++ src/object.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "object.h"
+
+OBJECT objs[] = {
+    {"a little cave", "cave", NULL},
+    {"an open field", "field", NULL},
+    {"a silver coin", "silver", field},
+    {"a gold coin", "gold", cave},
+    {"a burly guard", "guard", field},
+    {"yourself", "yourself", field}
+};
