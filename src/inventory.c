b7334df46902bc7f1bde285c3fb0965019341d45
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-03-23 11:04:25 +0530

    Add moveObject.

--- src/inventory.c
+++ src/inventory.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
 #include "object.h"
 #include "misc.h"
+
+static void moveObject(const char *noun, OBJECT *from, OBJECT *to)
+{
+    OBJECT *obj = parseObject(noun);
+    if (obj == NULL)
+    {
+        printf("I don't understand what item you mean.\n");
+    }
+    else if (from != obj->location)
+    {
+        printf("You can't.\n");
+    }
+    else if (to == NULL)
+    {
+        printf("There is nobody here to give that to.\n");
+    }
+    else
+    {
+        obj->location = to;
+        printf("OK.\n");
+    }
+}
